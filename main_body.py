6a118156c18d5b58eb34b8d866904a7d67bb9bf4
Zverev Dmitriy <zverev_armstrong@inbox.ru>
2023-02-28 12:25:04 +0700

    Main body refactor to class ed1_1

--- main_body.py
+++ main_body.py
@@ -180,5 +180,5 @@ def webhook():
     return "!", 200
 
 
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+# if __name__ == "__main__":
+#     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
